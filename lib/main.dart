639e9cfaac0c87d72f1bfd76e608ae17b1f24581
Lans12345 <olanalans12345@gmail.com>
2022-12-27 06:21:59 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:the_serve_admin/screens/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyB9YBhU-IkoYp-S-7UAq7_dUDdrYWJSklc",
+    appId: "1:629251471094:web:d4271deedfe5a5a0075ed7",
+    messagingSenderId: "629251471094",
+    projectId: "theserve-5c90b",
+  ));
   runApp(const MyApp());
 }
 
